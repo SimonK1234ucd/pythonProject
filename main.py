5513207a2d0ab08c16adb8075876b19ce1e047e5
otthot <otso22ab@student.cbs.dk>
2024-11-18 13:02:54 +0000

    Making layout changs foo

--- main.py
+++ main.py
@@ -103,7 +103,6 @@ with leftSide:
         st.altair_chart(chart)
 
     with compareCurrenciesTab:
-
         st.markdown("<p style='font-weight:bold'>Currency Converter</p>", unsafe_allow_html=True)
         st.markdown("<p style='font-size:14px'>The Currency Converter transforms a selected currency into other specified currencies.</p>", unsafe_allow_html=True)
 
@@ -141,89 +140,8 @@ with leftSide:
 
         st.caption(f"The bar chart displays the exchange rate of 1 {baseCurrency} of the base currency.")
         st.bar_chart(df.set_index("Currency")["Exchange Rate"], height=250)
+    
         
-    with currenciesHistoricallyTab:
-
-        st.markdown("<p style='font-weight:bold'>Compare Euro Historically</p>", unsafe_allow_html=True)
-        st.markdown("<p style='font-size:14px'>This tab offers statistics and graphical information on the historical performance of a selected currency against the Euro.</p>", unsafe_allow_html=True)
-
-        # Get the list of currencies
-        currencylistE=getreadfile.getcurrencylistE()
-
-        # Create two columns for side-by-side selection boxes
-        col1, col2 = st.columns(2)
-
-        # Box for selecting currency
-        with col1:
-            curE = st.selectbox("Select Currency to Compare", currencylistE, key = "tab2a")
-
-        # Dropdown for selecting time period (in the second column)
-        time_periods = ["YTD", "1 year", "2 years", "3 years", "5 years", "10 years", "20 years"]
-        with col2:
-            date = st.selectbox("Select Time Period", time_periods, key = "time_period") 
-        
-        # Set the current date explicitly for testing or real usage
-        current_date = pd.to_datetime("2024-11-13")
-
-        # Determine the start date based on the selected time period
-        periods = {
-            "YTD": current_date.replace(month=1, day=1),
-            "1 year": current_date - pd.DateOffset(years=1),
-            "2 years": current_date - pd.DateOffset(years=2),
-            "3 years": current_date - pd.DateOffset(years=3),
-            "5 years": current_date - pd.DateOffset(years=5),
-            "10 years": current_date - pd.DateOffset(years=10),
-            "20 years": current_date - pd.DateOffset(years=20),
-        }
-        start_date = periods.get(date, current_date)
-
-        
-        # Get the historical chart data for the selected currency
-        chartplot = getcurrencychart(curE)
-
-        # Filter the data based on the selected time period
-        chartplot = chartplot[chartplot.index >= start_date]
-
-        # Display the historical chart
-        st.write(f"Historical Data of {curE} for the Selected Time Period ({date})")
-        st.line_chart(chartplot)
-        
-        # Display currency risk assessment
-        st.markdown("<p style='font-weight:bold; font-size:22px'>Currency Risk Assessment:</p>", unsafe_allow_html=True)
-        display_currency_risk(curE, start_date)
-
-    with purchasingPowerTab:
-
-
-        st.markdown("<p style='font-weight:bold'>Purchasing Power Calculator</p>", unsafe_allow_html=True)
-        st.markdown("<p style='font-size:14px'>This tab provides comprehensive information for a clear overview of the purchasing power of a selected currency.</p>", unsafe_allow_html=True)
-       
-        check=st.radio("Select checkbox:", ["Historical Purchasing Power", "Future Purchasing Power"])
-        
-        if check=="Future Purchasing Power":
-            initial=st.number_input("Provide Initial Amount in EUR")
-            average=st.number_input("Provide Average Inflation Rate in %  (max. 20%)")
-            years=st.number_input("Provide Amount of Years  (max. 100)")
-
-            forprint=getPurchasingPower.getFuturePP(initial,average,years)
-            st.write(forprint)
-        
-        if check=="Historical Purchasing Power":
-            with st.expander("Settings"):
-                listcountrys=[]
-                listcountrys=getPurchasingPower.getHistoricalPPlist()
-                country=st.selectbox("Please Select the country of your interest:", listcountrys, key="tab3b")
-                initial=st.number_input("Provide Amount today")
-                years=st.text_input("What year do you like to know the Purchasing Power of? (YYYY)")
-
-            if country:
-                data3=getChartMPW.getchartforMPW(country)
-                st.write(f"Hisorical Money Purchasing Power of {country}: ")
-                st.line_chart(data3.set_index("Year"))
-            
-            if years and initial:
-                data3b=getPurchasingPower.getHistorialPPdata(country,years,initial)
-                st.write(f"The Value of {"{:.2f}".format(initial)} in {years} is {"{:.2f}".format(data3b)}")            
         
 with rightSide:
     # Changs this to a more correct labelling :)
@@ -271,8 +189,122 @@ with rightSide:
         st.markdown("<p style='font-weight:bold'>Exchange Spending Calculator</p>", unsafe_allow_html=True)
         st.markdown("<p style='font-size:14px'>This tab offers a comparison of living costs for exchange students.</p>", unsafe_allow_html=True)
     # Add content for the Buying Power Overview tab here
+    
     with beerTab:
+
         st.markdown("<p style='font-weight:bold'>BEER</p>", unsafe_allow_html=True)
         # Add content for the Buying Power Overview tab here
+
+    
+secondSection = st.container()
+with secondSection:
+    # Title of section...
+    st.markdown("<h1 style='font-weight:bold'>Currency Details</h1>", unsafe_allow_html=True)
+    #
+    upper = st.container()
+    lower = st.container()
+
+    # Add content for the Compare Currencies tab here
+    with upper:
+        [left, right] = st.columns(2)
+
+        with left:
+            st.write("Configure and compare currencies here by selecting the desired currency and time period.")
+            [firstColumn, secondColumn, thirdColumn] = st.columns(3)
+
+            with firstColumn:
+                #selectedCurrency = st.selectbox("Select Currency", currencyTypes)
+                st.write("This is the left side upper")
+                
+            with secondColumn:
+                timePeriod = st.selectbox("Select Time Period", ["1 year", "5 years", "10 years", "20 years"])
+
+            with thirdColumn:
+                st.write("This is the right side upper")
+
+        with right:
+            st.write("This is the right side upper")
+
+    with lower:
+        [left, right] = st.columns(2)
+        with left:
+            st.markdown("<p style='font-weight:bold'>Purchasing Power Calculator</p>", unsafe_allow_html=True)
+            st.markdown("<p style='font-size:14px'>This tab provides comprehensive information for a clear overview of the purchasing power of a selected currency.</p>", unsafe_allow_html=True)
+    
+            check=st.radio("Select checkbox:", ["Historical Purchasing Power", "Future Purchasing Power"])
+        
+            if check=="Future Purchasing Power":
+                initial=st.number_input("Provide Initial Amount in EUR")
+                average=st.number_input("Provide Average Inflation Rate in %  (max. 20%)")
+                years=st.number_input("Provide Amount of Years  (max. 100)")
+
+                forprint=getPurchasingPower.getFuturePP(initial,average,years)
+                st.write(forprint)
+        
+            if check=="Historical Purchasing Power":
+                with st.expander("Settings"):
+                    listcountrys=[]
+                    listcountrys=getPurchasingPower.getHistoricalPPlist()
+                    country=st.selectbox("Please Select the country of your interest:", listcountrys, key="tab3b")
+                    initial=st.number_input("Provide Amount today")
+                    years=st.text_input("What year do you like to know the Purchasing Power of? (YYYY)")
+
+            if country:
+                data3=getChartMPW.getchartforMPW(country)
+                st.write(f"Hisorical Money Purchasing Power of {country}: ")
+                st.line_chart(data3.set_index("Year"))
+            
+            if years and initial:
+                data3b=getPurchasingPower.getHistorialPPdata(country,years,initial)
+                st.write(f"The Value of {"{:.2f}".format(initial)} in {years} is {"{:.2f}".format(data3b)}") 
+
+        with right:
+            st.markdown("<p style='font-weight:bold'>Compare Euro Historically</p>", unsafe_allow_html=True)
+            st.markdown("<p style='font-size:14px'>This tab offers statistics and graphical information on the historical performance of a selected currency against the Euro.</p>", unsafe_allow_html=True)
+
+            # Get the list of currencies
+            currencylistE=getreadfile.getcurrencylistE()
+
+            # Create two columns for side-by-side selection boxes
+            col1, col2 = st.columns(2)
+
+            # Box for selecting currency
+            with col1:
+                curE = st.selectbox("Select Currency to Compare", currencylistE, key = "tab2a")
+
+            # Dropdown for selecting time period (in the second column)
+            time_periods = ["YTD", "1 year", "2 years", "3 years", "5 years", "10 years", "20 years"]
+            with col2:
+                date = st.selectbox("Select Time Period", time_periods, key = "time_period") 
+            
+            # Set the current date explicitly for testing or real usage
+            current_date = pd.to_datetime("2024-11-13")
+
+            # Determine the start date based on the selected time period
+            periods = {
+                "YTD": current_date.replace(month=1, day=1),
+                "1 year": current_date - pd.DateOffset(years=1),
+                "2 years": current_date - pd.DateOffset(years=2),
+                "3 years": current_date - pd.DateOffset(years=3),
+                "5 years": current_date - pd.DateOffset(years=5),
+                "10 years": current_date - pd.DateOffset(years=10),
+                "20 years": current_date - pd.DateOffset(years=20),
+            }
+            start_date = periods.get(date, current_date)
+
+            
+            # Get the historical chart data for the selected currency
+            chartplot = getcurrencychart(curE)
+
+            # Filter the data based on the selected time period
+            chartplot = chartplot[chartplot.index >= start_date]
+
+            # Display the historical chart
+            st.write(f"Historical Data of {curE} for the Selected Time Period ({date})")
+            st.line_chart(chartplot)
+            # Display currency risk assessment
+            # st.markdown("<p style='font-weight:bold; font-size:22px'>Currency Risk Assessment:</p>", unsafe_allow_html=True)
+            # display_currency_risk(curE, start_date)
+            
     
     
